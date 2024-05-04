0440448df1d5805282b881a73ce4b542ff5b32a9
Alex Chong <alex.chong@outlook.de>
2024-05-04 08:43:30 +0200

    binary search implemented
    started to implement the binary search. Currently working on the nested loop

--- Program.cs
+++ Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace
+namespace Converter
 {
-    class Test{
+    class BinaryConverter{
         public static void Main(string[] args)
         {
-            // Dezimal calc
+            Greeting();
+   
         }
+
+        // Method for calc
+        public static void Greeting()
+        {
+            Console.WriteLine($"Welcome to my Binary calculator");
+            Console.WriteLine($"First write the number to convert!");
+            
+            // User input var
+            string inputNumber = Console.ReadLine();
+            // string typeOfConversion = Console.ReadLine();
+
+            //Convert input to int
+            int numberOne = Convert.ToInt32(inputNumber);
+            
+            // Initial check for exponent
+            double maxExponent = Math.Log2(numberOne);
+            int roundedResult = Convert.ToInt32(Math.Floor(maxExponent));
+            Console.WriteLine($"The highest exponent is {roundedResult}");
+            
+        }
+
+
     }
 }
 
--- obj/Debug/net8.0/NumberConversion.AssemblyInfo.cs
+++ obj/Debug/net8.0/NumberConversion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberConversion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf436706cdac305a433f1fa5e08dd5f37a24a018")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberConversion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberConversion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
