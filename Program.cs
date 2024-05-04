bf436706cdac305a433f1fa5e08dd5f37a24a018
Alex Chong <alex.chong@outlook.de>
2024-05-04 06:14:41 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace
+{
+    class Test{
+        public static void Main(string[] args)
+        {
+            // Dezimal calc
+        }
+    }
+}
+
